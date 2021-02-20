91e1bc5350da8b0a2f36488d3b8e510d2dea5a24
kzresume <inz.zawisza.karol@gmail.com>
2021-02-20 18:25:38 +0100

    REDIS ADDED

--- resume/views.py
+++ resume/views.py
@@ -3,42 +3,57 @@ from .models import Post, Attribute
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.utils import timezone
+import redis
+import inspect
+
+r = redis.StrictRedis(port =6379  ,host ='localhost' ,db = 0)
+TODAY = str(timezone.now().date())
+ 
 
 def home(request):
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'main.html',{} )
 
 def education(request):
     data = Attribute.objects.get(name='Education')
     context = {'selected':'Education','data':data}
+    r.incr(inspect.currentframe().f_code.co_name )
     return render(request, 'education.html',context)
 
 def expirience(request):
     data = Attribute.objects.get(name='Expirience')
     context = {'selected':'Expirience','data':data}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'expirience.html',context)
 
 def about_me(request):
     data = Attribute.objects.get(name='About_me')
     context = {'selected':'About_me','data':data}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'about_me.html',context)
 
 def back(request):
     data = Attribute.objects.get(name='Back-end')
     context = {'selected':'Back','data':data}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'back-end.html',context)
 
 def website(request):
     data = Attribute.objects.get(name='Website')
     context = {'selected':'Website','data':data}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'website.html',context)
 
 def contact(request):
     data = Attribute.objects.get(name='Contact')
     context = {'selected':'Contact','data':data}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'contact.html',context)
 
 def api_list(request):
     context = {'selected':'Api'}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'api.html',context)
 
 
@@ -48,6 +63,7 @@ def post_detail(request, year, month, day, post):
     print(post)
     template = 'blog_detail.html'
     context = {'post': post}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, template, context)
 
 def skills(request, tag_slug=None):
@@ -65,4 +81,5 @@ def skills(request, tag_slug=None):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     context = {'page': page, 'posts': posts, 'tag':tag,'selected':'Skills'}
+    r.incr(inspect.currentframe().f_code.co_name +":"+ TODAY)
     return render(request, 'skills.html', context )
